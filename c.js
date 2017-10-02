45097675cd2d80c6e97ee005a204bc2d6a8f154f
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-02 18:44:56 +0700

    cong nhan chia

--- c.js
+++ c.js
@@ -10,7 +10,23 @@ function cong(a, b, cb) {
     });
 }
 
-cong(5, 10, (err, kq) => {
-    if(err) return console.log(err.message);
-    console.log(kq);
-});
+function nhan(a, b, cb) {
+    const isNumbers = typeof a === 'number' && typeof b === 'number';
+    if (!isNumbers) return cb(new Error('Type error'));
+    const url = `http://localhost:3000/tinh/NHAN/${a}/${b}`;
+    request(url, (err, response, body) => {
+        if (err) return cb(err);
+        cb(null, body);
+    });
+}
+
+function chia(a, b, cb) {
+    const isNumbers = typeof a === 'number' && typeof b === 'number';
+    if (!isNumbers) return cb(new Error('Type error'));
+    if (b === 0) return cb(new Error('Divide by 0'));
+    const url = `http://localhost:3000/tinh/CONG/${a}/${b}`;
+    request(url, (err, response, body) => {
+        if (err) return cb(err);
+        cb(null, body);
+    });
+}
