9e9fa9ab90133dc8f4638b3564d5c3e77f1f5a5b
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-02 18:10:34 +0700

    fn

--- b.js
+++ b.js
@@ -2,7 +2,18 @@ const request = require('request');
 
 const url = 'http://api.openweathermap.org/data/2.5/weather?appid=01cc37655736835b0b75f2b395737694&units=metric&q=';
 
-request(url + 'Saigon', (err, response, body) => {
-    const temp = JSON.parse(body).main.temp;
-    console.log(temp);
-});
+// request(url + 'Saigon', (err, response, body) => {
+//     temp = JSON.parse(body).main.temp;
+// });
+
+function getTempByCityName(cityName, cb) {
+    request(url + cityName, (err, response, body) => {
+        temp = JSON.parse(body).main.temp;
+        cb(temp);
+    });
+}
+
+getTempByCityName('Saigon', temp => console.log(temp));
+getTempByCityName('Hanoi', t => console.log('Hanoi: ' + t));
+// getTempByCityName('Tokyo');
+// console.log(sgTemp);
