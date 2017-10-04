dadf585c3ee58be8a6de75ecd9cbf617de61e744
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-04 17:42:59 +0700

    review

--- c.js
+++ c.js
@@ -6,7 +6,7 @@ function cong(a, b, cb) {
     const url = `http://localhost:3000/tinh/CONG/${a}/${b}`;
     request(url, (err, response, body) => {
         if (err) return cb(err);
-        cb(null, body);
+        cb(null, +body);
     });
 }
 
@@ -16,7 +16,7 @@ function nhan(a, b, cb) {
     const url = `http://localhost:3000/tinh/NHAN/${a}/${b}`;
     request(url, (err, response, body) => {
         if (err) return cb(err);
-        cb(null, body);
+        cb(null, +body);
     });
 }
 
@@ -24,9 +24,24 @@ function chia(a, b, cb) {
     const isNumbers = typeof a === 'number' && typeof b === 'number';
     if (!isNumbers) return cb(new Error('Type error'));
     if (b === 0) return cb(new Error('Divide by 0'));
-    const url = `http://localhost:3000/tinh/CONG/${a}/${b}`;
+    const url = `http://localhost:3000/tinh/CHIA/${a}/${b}`;
     request(url, (err, response, body) => {
         if (err) return cb(err);
-        cb(null, body);
+        cb(null, +body);
     });
 }
+
+function tinhDienTichHinhThang(a, b, h, cb) {
+    cong(a, b, (errCong, tong) => {
+        if (errCong) return cb(errCong);
+        nhan(tong, h, (errNhan, tich) => {
+            if (errNhan) return cb(errNhan);
+            chia(tich, 2, (errChia, kq) => {
+                if (errChia) return cb(errChia);
+                cb(null, kq);
+            });
+        });
+    });
+}
+
+tinhDienTichHinhThang(4, 5, 6, (err, kq) => console.log(kq));
